7770cf056bd19c53b56cd20354c195502aafbde0
Matthew Hartley <mhartley@cantab.net>
2018-08-05 22:46:27 +0100

    Add tile generators

--- dmlutils/__init__.py
+++ dmlutils/__init__.py
@@ -1,8 +1,22 @@
 
+from imageio import imread
+
+
+def identifiers_where_overlay_is_true(dataset, overlay_name):
+
+    overlay = dataset.get_overlay(overlay_name)
+
+    selected = [identifier
+                for identifier in dataset.identifiers
+                if overlay[identifier]]
+
+    return selected
+
 
 def tile_generator(im, ts=256):
 
-    xdim, ydim, _ = im.shape
+    xdim, ydim = im.shape[0], im.shape[1]
+
     nx = xdim//ts
     ny = ydim//ts
 
@@ -10,3 +24,16 @@ def tile_generator(im, ts=256):
         for y in range(ny):
             itile = im[x*ts:(x+1)*ts,y*ts:(y+1)*ts]
             yield itile
+
+
+def dataset_tile_generator(dataset, ts=256):
+
+    mask_ids = dataset.get_overlay("mask_ids")
+    for imid in identifiers_where_overlay_is_true(dataset, "is_image"):
+        im = imread(dataset.item_content_abspath(imid))
+        mask = imread(dataset.item_content_abspath(mask_ids[imid]))
+
+        imgen = tile_generator(im, ts=ts)
+        maskgen = tile_generator(mask, ts=ts)
+        for itile, mtile in zip(imgen, maskgen):
+            yield itile, mtile
--- tests/test_tile_generation.py
+++ tests/test_tile_generation.py
@@ -1,6 +1,11 @@
 
 import os
 
+import pytest
+import dtoolcore
+
+import numpy as np
+
 from imageio import imread
 
 
@@ -21,3 +26,26 @@ def test_tile_generation():
 	tgen = tile_generator(im, ts=512)
 	tile = next(tgen)
 	assert tile.shape == (512, 512, 3)
+
+	tgen = tile_generator(im, ts=512)
+	for x in range(15):
+		next(tgen)
+
+	with pytest.raises(StopIteration):
+		next(tgen)
+
+
+def test_dataset_tile_generation():
+
+	from dmlutils import dataset_tile_generator
+
+	ds_path = os.path.join(TEST_SAMPLE_DATA, 'test_tile_generator')
+	dataset = dtoolcore.DataSet.from_uri(ds_path)
+	dgen = dataset_tile_generator(dataset)
+	
+	image, mask = next(dgen)
+	assert image.shape == (256, 256, 3)
+	assert mask.shape == (256, 256)
+	im = imread(os.path.join(TEST_SAMPLE_DATA, '000.png'))
+	assert np.array_equal(image, im[:256,:256,:])
+
